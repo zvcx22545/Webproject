4c6c2323ed5d33d8e8df06edbf1e3a21ba6e30e6
Sittsak Phimthongphum <bigkub@Sittisaks-MacBook-Air-4.local>
2024-05-31 02:58:00 +0700

    recode db

--- index/config/db.php
+++ index/config/db.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=web_project;port=8889;", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=web_project_test", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
